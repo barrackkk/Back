6937fe344b76429cdc005f93d31bf20ed814970c
barrackkk <kgmlwns904@gmail.com>
2025-08-31 14:53:30 +0900

    feat: UserController Patch 요청 기능 구현

--- src/main/java/com/fitpet/server/domain/user/controller/UserController.java
+++ src/main/java/com/fitpet/server/domain/user/controller/UserController.java
@@ -2,6 +2,7 @@ package com.fitpet.server.domain.user.controller;
 
 import com.fitpet.server.domain.user.dto.UserCreateRequest;
 import com.fitpet.server.domain.user.dto.UserDto;
+import com.fitpet.server.domain.user.dto.UserUpdateRequest;
 import com.fitpet.server.domain.user.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -9,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -48,6 +50,21 @@ public class UserController {
         UserDto user = userService.findUser(userId);
 
         log.info("[UserController] 사용자 조회 완료: id: {}", userId);
+
+        return ResponseEntity.status(HttpStatus.OK).body(user);
+
+    }
+
+    @PatchMapping("/{userId}")
+    public ResponseEntity<UserDto> update(
+        @PathVariable Long userId,
+        @RequestBody UserUpdateRequest userUpdateRequest
+    ) {
+        log.info("[UserController] 사용자 수정 요청: id: {}", userId);
+
+        UserDto user = userService.updateUser(userId, userUpdateRequest);
+
+        log.info("[UserController] 사용자 수정 완료: id: {}", userId);
 
         return ResponseEntity.status(HttpStatus.OK).body(user);
 
