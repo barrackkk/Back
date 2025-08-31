7d0aeaf94648587a48a592a6608321c0833a7d68
barrackkk <kgmlwns904@gmail.com>
2025-08-31 13:16:15 +0900

    feat: UserController Post 요청 구현

---
+++ src/main/java/com/fitpet/server/domain/user/controller/UserController.java
@@ -0,0 +1,39 @@
+package com.fitpet.server.domain.user.controller;
+
+import com.fitpet.server.domain.user.dto.UserCreateRequest;
+import com.fitpet.server.domain.user.dto.UserDto;
+import com.fitpet.server.domain.user.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/users")
+@Slf4j
+public class UserController {
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<UserDto> create(
+        @Valid @RequestBody UserCreateRequest userCreateRequest
+    ) {
+        log.info("[UserController] 사용자 회원가입 요청: email: {}, nickname: {}",
+            userCreateRequest.email(), userCreateRequest.nickname());
+
+        UserDto createdUser = userService.createUser(userCreateRequest);
+
+        log.info("[UserController] 사용자 회원가입 완료: id: {}, email: {}",
+            createdUser.userId(), createdUser.email());
+
+        return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .body(createdUser);
+    }
+}
