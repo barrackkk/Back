2ba000af6ae581bc5207e8fb04bc848553d3e665
barrackkk <kgmlwns904@gmail.com>
2025-08-31 13:16:31 +0900

    refactor: 필드에 맞게 UserDto 수정

--- src/main/java/com/fitpet/server/domain/user/dto/UserDto.java
+++ src/main/java/com/fitpet/server/domain/user/dto/UserDto.java
@@ -1,12 +1,22 @@
 package com.fitpet.server.domain.user.dto;
 
-import java.util.UUID;
+import com.fitpet.server.domain.user.entity.Gender;
+import java.time.LocalDateTime;
 import lombok.Builder;
 
 @Builder
 public record UserDto(
-    UUID userId,
+    Long userId,
+    String email,
     String nickname,
-    String email
+    Integer age,
+    Gender gender,
+    Double weightKg,
+    Double targetWeightKg,
+    Double heightCm,
+    Double pbf,
+    Double targetPbf,
+    LocalDateTime createdAt,
+    LocalDateTime updatedAt
 ) {
 }
