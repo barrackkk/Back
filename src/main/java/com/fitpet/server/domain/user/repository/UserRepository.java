24aab9e5336b8482862ac543daf24453b9af0825
barrackkk <kgmlwns904@gmail.com>
2025-08-31 13:14:25 +0900

    refactor: 메서드명 변경
    - find -> exist

--- src/main/java/com/fitpet/server/domain/user/repository/UserRepository.java
+++ src/main/java/com/fitpet/server/domain/user/repository/UserRepository.java
@@ -1,14 +1,13 @@
 package com.fitpet.server.domain.user.repository;
 
 import com.fitpet.server.domain.user.entity.User;
-import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, UUID> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
-    boolean findByEmail(String email);
+    boolean existsByEmail(String email);
 
-    boolean findByNickName(String nickName);
+    boolean existsByNickname(String nickName);
 }
--- src/main/java/com/fitpet/server/domain/user/service/UserServiceImpl.java
+++ src/main/java/com/fitpet/server/domain/user/service/UserServiceImpl.java
@@ -5,22 +5,21 @@ import com.fitpet.server.domain.user.dto.UserDto;
 import com.fitpet.server.domain.user.entity.User;
 import com.fitpet.server.domain.user.mapper.UserMapper;
 import com.fitpet.server.domain.user.repository.UserRepository;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-@NoArgsConstructor
 @Slf4j
 public class UserServiceImpl implements UserService {
 
-    private static UserRepository userRepository;
-    private static UserMapper userMapper;
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
 
     @Override
-    public UserDto createUser(UserCreateRequest request) throws IllegalAccessException {
+    public UserDto createUser(UserCreateRequest request) {
         log.debug("[UserService]: 사용자 등록 요청 - UserCreateRequest: {}", request);
 
         validateUserCreateRequest(request);
@@ -28,19 +27,18 @@ public class UserServiceImpl implements UserService {
         User user = userMapper.toEntity(request);
         User savedUser = userRepository.save(user);
 
-        log.info("[UserService]: 사용자 등록 완료: id={}, nickname={}", savedUser.getId(), savedUser.getNickName());
+        log.info("[UserService]: 사용자 등록 완료: id={}, nickname={}", savedUser.getId(), savedUser.getNickname());
 
         return userMapper.toDto(savedUser);
 
     }
 
-    public void validateUserCreateRequest(UserCreateRequest request) throws IllegalAccessException {
-        if (userRepository.findByEmail(request.email())) {
-            throw new IllegalAccessException("이미 존재하는 이메일입니다.");
+    public void validateUserCreateRequest(UserCreateRequest request) {
+        if (userRepository.existsByEmail(request.email())) {
+            throw new IllegalArgumentException("이미 존재하는 이메일입니다.");
         }
-
-        if (userRepository.findByNickName(request.nickname())) {
-            throw new IllegalAccessException("이미 존재하는 닉네임입니다.");
+        if (userRepository.existsByNickname(request.nickname())) {
+            throw new IllegalArgumentException("이미 존재하는 닉네임입니다.");
         }
 
     }
