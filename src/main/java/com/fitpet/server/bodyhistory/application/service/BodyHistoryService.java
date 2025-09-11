8ebbd9adfe048962312b490134bc1ce56e17558d
최경영 <yeong7098@naver.com>
2025-09-11 22:54:57 +0900

    Feat : createRequest DTO 생성

--- src/main/java/com/fitpet/server/bodyhistory/application/service/BodyHistoryService.java
+++ src/main/java/com/fitpet/server/bodyhistory/application/service/BodyHistoryService.java
@@ -1,20 +1,22 @@
-package com.fitpet.server.bodyhistory.domain.repository;
+package com.fitpet.server.bodyhistory.application.service;
+
 
 import com.fitpet.server.bodyhistory.domain.entity.BodyHistory;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.validation.annotation.Validated;
 
-public interface BodyHistoryRepository {
+@Validated
+public interface BodyHistoryService {
     BodyHistory save(BodyHistory bodyHistory);
 
     Optional<BodyHistory> findById(Long id);
 
     List<BodyHistory> findAllByUserId(Long userId);
 
+    // Date는 월 단위로 검색
     List<BodyHistory> findAllByUserIdAndDate(Long userId, LocalDate startDate, LocalDate endDate);
 
     void deleteById(Long id);
-
-    boolean existsById(Long id);
 }
