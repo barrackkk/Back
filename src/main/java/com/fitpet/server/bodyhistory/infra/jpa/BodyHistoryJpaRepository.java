27102d1d2a2db0a256e89c12c0bd48e2400c8cc4
최경영 <yeong7098@naver.com>
2025-09-12 17:27:16 +0900

    Chore : [Controller] 메소드명, 반환형식 수정

--- src/main/java/com/fitpet/server/bodyhistory/infra/jpa/BodyHistoryJpaRepository.java
+++ src/main/java/com/fitpet/server/bodyhistory/infra/jpa/BodyHistoryJpaRepository.java
@@ -1,11 +1,13 @@
 package com.fitpet.server.bodyhistory.infra.jpa;
 
-import com.fitpet.server.bodyhistory.domain.entity.BodyHistory;
 import java.time.LocalDate;
 import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+
+import com.fitpet.server.bodyhistory.domain.entity.BodyHistory;
 
 public interface BodyHistoryJpaRepository extends JpaRepository<BodyHistory, Long> {
 
