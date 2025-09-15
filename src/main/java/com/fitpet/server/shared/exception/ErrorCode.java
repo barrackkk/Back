4507cba78d439408e8b4aaa0a74d99ba47389d7a
barrackkk <kgmlwns904@gmail.com>
2025-09-15 10:28:48 +0900

    Feat: 소셜로그인관련 커스텀 예외처리 추가

--- src/main/java/com/fitpet/server/shared/exception/ErrorCode.java
+++ src/main/java/com/fitpet/server/shared/exception/ErrorCode.java
@@ -1,8 +1,7 @@
 package com.fitpet.server.shared.exception;
 
-import org.springframework.http.HttpStatus;
-
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 public enum ErrorCode {
@@ -22,7 +21,10 @@ public enum ErrorCode {
 
     INVALID_LOGIN(HttpStatus.UNAUTHORIZED, "A001", "아이디 또는 비밀번호가 일치하지 않습니다."),
     INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "A002", "유효하지 않은 리프레시 토큰입니다."),
-    INVALID_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "A003", "유효하지 않은 액세스 토큰입니다.");
+    INVALID_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "A003", "유효하지 않은 액세스 토큰입니다."),
+    OAUTH_INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "A004", "유효하지 않은 소셜 토큰입니다."),
+    OAUTH_VERIFICATION_FAILED(HttpStatus.BAD_GATEWAY, "A005", "소셜 토큰 검증에 실패했습니다."),
+    OAUTH_PROFILE_NOT_FOUND(HttpStatus.BAD_GATEWAY, "A006", "소셜 사용자 정보를 확인할 수 없습니다.");
 
 
     private final HttpStatus status;
