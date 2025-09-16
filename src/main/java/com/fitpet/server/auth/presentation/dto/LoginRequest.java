accfe3669db706cefbe131ac5f69e1a95de0c9ca
barrackkk <kgmlwns904@gmail.com>
2025-09-16 14:24:22 +0900

    Feat: 유효성 검사 추가

--- src/main/java/com/fitpet/server/auth/presentation/dto/LoginRequest.java
+++ src/main/java/com/fitpet/server/auth/presentation/dto/LoginRequest.java
@@ -2,10 +2,11 @@ package com.fitpet.server.auth.presentation.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record LoginRequest(
-        @Email String email,
-        @NotBlank String password
+    @NotBlank @Email String email,
+    @NotBlank @Size(min = 8, max = 72, message = "8자 이상이어야 합니다.") String password
 ) {
 }
 
