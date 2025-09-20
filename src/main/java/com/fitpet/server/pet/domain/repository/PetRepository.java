0c490bfdada1b773e062c05ad05579696e5513a0
barrackkk <kgmlwns904@gmail.com>
2025-09-20 18:48:34 +0900

    Feat: Pet 찾는 로직 및 삭제 로직 추가

--- src/main/java/com/fitpet/server/pet/domain/repository/PetRepository.java
+++ src/main/java/com/fitpet/server/pet/domain/repository/PetRepository.java
@@ -1,9 +1,14 @@
 package com.fitpet.server.pet.domain.repository;
 
 import com.fitpet.server.pet.domain.entity.Pet;
+import java.util.Optional;
 
 public interface PetRepository {
     Pet save(Pet pet);
 
     boolean existsByOwnerId(Long ownerId);
+
+    Optional<Pet> findById(Long petId);
+
+    void delete(Pet pet);
 }
--- src/main/java/com/fitpet/server/pet/infra/adapter/PetRepositoryAdapter.java
+++ src/main/java/com/fitpet/server/pet/infra/adapter/PetRepositoryAdapter.java
@@ -3,6 +3,7 @@ package com.fitpet.server.pet.infra.adapter;
 import com.fitpet.server.pet.domain.entity.Pet;
 import com.fitpet.server.pet.domain.repository.PetRepository;
 import com.fitpet.server.pet.infra.jpa.PetJpaRepository;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -19,6 +20,16 @@ public class PetRepositoryAdapter implements PetRepository {
     @Override
     public boolean existsByOwnerId(Long ownerId) {
         return jpa.existsByOwnerId(ownerId);
+    }
+
+    @Override
+    public Optional<Pet> findById(Long petId) {
+        return jpa.findById(petId);
+    }
+
+    @Override
+    public void delete(Pet pet) {
+        jpa.delete(pet);
     }
 
 }
