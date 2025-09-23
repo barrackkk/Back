8d1bf3f4bbf779e8afc69653ae821f0632bae101
최경영 <yeong7098@naver.com>
2025-09-23 23:27:32 +0900

    Chore : JpaConfig 생성 및 @EnableJpaAuditing 이동

--- src/main/java/com/fitpet/server/FitPetApplication.java
+++ src/main/java/com/fitpet/server/FitPetApplication.java
@@ -2,10 +2,8 @@ package com.fitpet.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class FitPetApplication {
 
     public static void main(String[] args) {
