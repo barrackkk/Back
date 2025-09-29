2905d4f2160869a78a2e2ac49facfc3b96b7f9cb
barrackkk <kgmlwns904@gmail.com>
2025-09-29 18:52:24 +0900

    chore: UserInputInfoRequest 유효성 추가

--- src/main/java/com/fitpet/server/user/presentation/dto/UserInputInfoRequest.java
+++ src/main/java/com/fitpet/server/user/presentation/dto/UserInputInfoRequest.java
@@ -1,26 +1,37 @@
 package com.fitpet.server.user.presentation.dto;
 
 import com.fitpet.server.user.domain.entity.Gender;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import org.hibernate.validator.constraints.Length;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 
 public record UserInputInfoRequest(
-    @NotNull
-    @Length(min = 5, max = 20, message = "닉네임은 5자 이상 20자 미만이어야 합니다.")
+    @NotBlank(message = "닉네임은 공백일 수 없습니다.")
+    @Size(min = 5, max = 20, message = "닉네임은 5자 이상 20자 이하여야 합니다.")
     String nickname,
 
-    @NotNull
+    @Min(value = 1, message = "나이는 1세 이상이어야 합니다.")
+    @Max(value = 120, message = "나이는 150세 이하여야 합니다.")
     int age,
 
     @NotNull
     Gender gender,
 
     @NotNull
+    @Positive(message = "몸무게(kg)는 0보다 커야 합니다.")
     Double weightKg,
+
     @NotNull
+    @Positive(message = "키(cm)는 0보다 커야 합니다.")
     Double heightCm,
+
     Double targetWeightKg,
+
     Double pbf,
+
     Double targetPbf
 ) {
 }
