0aa32ae8d29b067f163348e635a405fe0f657d40
barrackkk <kgmlwns904@gmail.com>
2025-10-16 17:25:15 +0900

    chore: DTO petType 데이터타입 일치

--- src/main/java/com/fitpet/server/pet/presentation/dto/PetCreateRequest.java
+++ src/main/java/com/fitpet/server/pet/presentation/dto/PetCreateRequest.java
@@ -1,11 +1,12 @@
 package com.fitpet.server.pet.presentation.dto;
 
+import com.fitpet.server.pet.domain.entity.PetType;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record PetCreateRequest(
-    @NotBlank String name,
-    @NotNull String petType, // enum 문자열로 받음: "DOG", "CAT"
-    @NotNull String color
+        @NotBlank String name,
+        @NotNull PetType petType,
+        @NotNull String color
 ) {
 }
