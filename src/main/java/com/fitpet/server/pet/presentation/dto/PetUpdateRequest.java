bf3b26713cd48127b434b75cb9d32ca85d4d2785
barrackkk <kgmlwns904@gmail.com>
2025-10-16 17:01:30 +0900

    feat: update Dto 추가

---
+++ src/main/java/com/fitpet/server/pet/presentation/dto/PetUpdateRequest.java
@@ -0,0 +1,12 @@
+package com.fitpet.server.pet.presentation.dto;
+
+import com.fitpet.server.pet.domain.entity.PetType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record PetUpdateRequest(
+        @NotBlank String name,
+        @NotNull PetType petType,
+        @NotNull String color
+) {
+}
