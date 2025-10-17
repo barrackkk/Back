68e00deef2cb2a57bce2f2c0dea83180e338246f
최경영 <yeong7098@naver.com>
2025-10-18 01:25:47 +0900

    Chore : controller 수정

--- src/main/java/com/fitpet/server/report/presentation/controller/ReportController.java
+++ src/main/java/com/fitpet/server/report/presentation/controller/ReportController.java
@@ -1,8 +1,8 @@
 package com.fitpet.server.report.presentation.controller;
 
 import com.fitpet.server.report.application.service.ReportService;
-import com.fitpet.server.report.presentation.dto.response.ReportResponseDto.DailyWalkSummaryResponse;
-import com.fitpet.server.report.presentation.dto.response.ReportResponseDto.TodayWorkoutResponse;
+import com.fitpet.server.report.presentation.dto.response.ReportResponseDto.ActivityRangeResponse;
+import com.fitpet.server.report.presentation.dto.response.ReportResponseDto.TodayActivityResponse;
 import com.fitpet.server.shared.security.UserDetailsImpl;
 import java.time.LocalDate;
 import java.util.List;
@@ -11,33 +11,29 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/reports")
 @RequiredArgsConstructor
 public class ReportController {
 
     private final ReportService reportService;
 
-    @GetMapping("/today-workout")
-    public ResponseEntity<List<TodayWorkoutResponse>> getTodayWorkouts(
+    @GetMapping("/activity/daily")
+    public ResponseEntity<TodayActivityResponse> getTodayActivity(
             @AuthenticationPrincipal UserDetailsImpl userDetails,
             @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
-
-        List<TodayWorkoutResponse> response = reportService.getTodayWorkouts(userDetails.getUserId(), date);
+        TodayActivityResponse response = reportService.getTodayActivity(userDetails.getUserId(), date);
         return ResponseEntity.ok(response);
     }
 
     @GetMapping("/activity/range")
-    public ResponseEntity<List<DailyWalkSummaryResponse>> getWeeklyWalks(
+    public ResponseEntity<List<ActivityRangeResponse>> getActivityRange(
             @AuthenticationPrincipal UserDetailsImpl userDetails,
             @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate from,
-            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate to
-    ) {
-        List<DailyWalkSummaryResponse> response = reportService.getWeeklyWalks(userDetails.getUserId(), from, to);
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate to) {
+        List<ActivityRangeResponse> response = reportService.getActivityRange(userDetails.getUserId(), from, to);
         return ResponseEntity.ok(response);
     }
 }
