16ee9e7a645fe4d6d6a5010288a9a4d04302b954
barrackkk <kgmlwns904@gmail.com>
2025-10-21 14:05:01 +0900

    feat: 매일 00시 초기화 스케쥴 추가

---
+++ src/main/java/com/fitpet/server/shared/config/SchedulerConfig.java
@@ -0,0 +1,9 @@
+package com.fitpet.server.shared.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulerConfig {
+}
