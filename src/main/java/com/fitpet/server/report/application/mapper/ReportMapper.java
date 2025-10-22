f3f7a6cde8b45550b586088c4070301f054dce0e
최경영 <yeong7098@naver.com>
2025-10-22 23:37:06 +0900

    Chore : Mapper 수정

--- src/main/java/com/fitpet/server/report/application/mapper/ReportMapper.java
+++ src/main/java/com/fitpet/server/report/application/mapper/ReportMapper.java
@@ -2,7 +2,11 @@ package com.fitpet.server.report.application.mapper;
 
 import com.fitpet.server.dailywalk.domain.entity.DailyWalk;
 import com.fitpet.server.report.presentation.dto.response.ReportResponseDto.ActivityRangeResponse;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(
@@ -10,5 +14,19 @@ import org.mapstruct.ReportingPolicy;
         unmappedTargetPolicy = ReportingPolicy.IGNORE
 )
 public interface ReportMapper {
+
+
+    @Mapping(source = "step", target = "steps")
+    @Mapping(source = "createdAt", target = "date", qualifiedByName = "toLocalDate")
     ActivityRangeResponse toActivityRangeResponse(DailyWalk dailyWalk);
+
+
+    @Named("toLocalDate")
+    default LocalDate toLocalDate(LocalDateTime dateTime) {
+        if (dateTime == null) {
+            return null;
+        }
+        return dateTime.toLocalDate();
+    }
+
 }
