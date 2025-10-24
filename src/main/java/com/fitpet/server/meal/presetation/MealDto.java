d53e742a9b9845b22c2017f5705457e3e11295ee
최경영 <yeong7098@naver.com>
2025-10-24 22:04:05 +0900

    Feat : DTO 생성

---
+++ src/main/java/com/fitpet/server/meal/presetation/MealDto.java
@@ -0,0 +1,52 @@
+package com.fitpet.server.meal.presentation.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import java.time.LocalDate;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+public class MealDto {
+
+    @Getter
+    public static class CreateRequest {
+        @NotNull
+        @PastOrPresent
+        @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+        private LocalDate date;
+        @NotBlank
+        private String title;
+        @NotNull
+        private Integer kcal;
+        @NotNull
+        private Integer sequence;
+    }
+
+    @Getter
+    @Builder
+    public static class CreateResponse {
+        private Long mealId;
+        private String imageUrl;
+        private String uploadUrl;
+    }
+
+    @Getter
+    public static class UpdateRequest {
+        @NotBlank
+        private String title;
+        @NotNull
+        private Integer kcal;
+        @NotNull
+        private Integer sequence;
+        private Boolean changeImage = false;
+    }
+
+    @Getter
+    @Builder
+    public static class UpdateResponse {
+        private String imageUrl;
+        private String uploadUrl;
+    }
+}
