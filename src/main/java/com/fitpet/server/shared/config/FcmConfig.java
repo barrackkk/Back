3b7b82ccb30b8b0e1046763ae84f89eee4d57b08
최경영 <yeong7098@naver.com>
2025-11-06 00:26:33 +0900

    Feat : FcmConfig 생생

---
+++ src/main/java/com/fitpet/server/shared/config/FcmConfig.java
@@ -0,0 +1,53 @@
+package com.fitpet.server.shared.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
+import jakarta.annotation.PostConstruct;
+import java.io.FileInputStream;
+import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+public class FcmConfig {
+
+    @Value("${fcm.service-account-key-path}")
+    private String serviceAccountKeyPath;
+
+    private FirebaseApp firebaseApp;
+
+    @PostConstruct
+    public void initialize() {
+        try (FileInputStream serviceAccountStream = new FileInputStream(serviceAccountKeyPath)) {
+
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccountStream))
+                    .build();
+
+            if (FirebaseApp.getApps().isEmpty()) {
+                this.firebaseApp = FirebaseApp.initializeApp(options);
+                log.info("FirebaseApp 초기화 성공");
+            } else {
+                this.firebaseApp = FirebaseApp.getInstance();
+                log.info("FirebaseApp 이미 초기화됨");
+            }
+
+        } catch (IOException e) {
+            log.error("FCM 키 파일을 찾을 수 없거나 읽는 데 실패했습니다. (경로: {}): {}", serviceAccountKeyPath, e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Bean
+    public FirebaseMessaging firebaseMessaging() {
+        if (this.firebaseApp == null) {
+            throw new IllegalStateException("FirebaseApp이 초기화되어야 합니다.");
+        }
+        return FirebaseMessaging.getInstance(this.firebaseApp);
+    }
+}
