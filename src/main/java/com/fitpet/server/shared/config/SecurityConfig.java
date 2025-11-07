d6f18c061cad7f7ecc4d43811344073a1b68e0b3
barrackkk <kgmlwns904@gmail.com>
2025-11-07 12:52:16 +0900

    chore: 미션 요청 PERMIT_URL_ARRAY 추가

--- src/main/java/com/fitpet/server/shared/config/SecurityConfig.java
+++ src/main/java/com/fitpet/server/shared/config/SecurityConfig.java
@@ -29,7 +29,8 @@ public class SecurityConfig {
             "/body-histories/**",
             "/actuator/**",
             "/gps/**",
-            "/pets/**"
+            "/pets/**",
+            "/missions/**"
     };
 
     @Bean
