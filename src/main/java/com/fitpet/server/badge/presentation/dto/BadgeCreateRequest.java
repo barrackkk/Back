abd7a531a0449b114617702311d3a0279cb8bb12
barrackkk <kgmlwns904@gmail.com>
2025-11-24 14:51:11 +0900

    chore: @NotBlank 추가

--- src/main/java/com/fitpet/server/badge/presentation/dto/BadgeCreateRequest.java
+++ src/main/java/com/fitpet/server/badge/presentation/dto/BadgeCreateRequest.java
@@ -1,10 +1,11 @@
 package com.fitpet.server.badge.presentation.dto;
 
 import com.fitpet.server.badge.domain.entity.BadgeType;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record BadgeCreateRequest(
-        @NotNull String title,
+        @NotNull @NotBlank String title,
         @NotNull BadgeType type,
         Integer conditionDuration,
         Long conditionGoal,
